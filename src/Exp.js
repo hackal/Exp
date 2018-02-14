ca088679993360e7f1746d494da156c1294f3274
Adam Pavlisin <adam.pavlisin@exponea.com>
2018-02-14 11:16:42 +0000

    Add support for more input types, html/css embeding, mounted method, css scoping

--- src/Exp.js
+++ src/Exp.js
@@ -1,13 +1,25 @@
 class Exp {
     constructor(settings) {
         /* Find application element */
-        this.app = document.querySelector(settings.el);
+        this.el = settings.el || null;
+        this.app = null;
+        this.attatch = settings.attatch || null;
+
+        /* Exp handling HTMl */
+        this.html = settings.html || null;
+
+        /* Exp handling styling */
+        this.style = settings.style || null;
+        this.scoped = settings.scoped || false;
 
         this.data = settings.data;
-        this.methods = settings.methods;
+        this.methods = settings.methods || null;
+        this.mounted = settings.mounted || null;
 
         /* init model */
         this.model = {};
+
+        this.init();
 
         /* init watcher */
         this.watcher(this.data);
@@ -19,11 +31,101 @@ class Exp {
         /* move methods to model for outside use */
         this.moveMethods();
 
+        this.loaded();
         return this.model;
     }
 
+    init() {
+        if (this.el !== null) {
+            this.app = document.querySelector(this.el);
+        } else if (this.html !== null) {
+            let el = document.createElement('div');
+            el.innerHTML = this.html.trim();
+            if (this.attatch !== null) {
+                this.app = document.querySelector(this.attatch).appendChild(el.firstChild).parentNode;
+            } else {
+                this.app = document.body.appendChild(el);
+            }
+        }
+
+        if (this.style !== null) {
+            if (this.scoped) {
+                let style = this.addStyle(this.style, true)
+                let rules = this.listify(style.sheet.cssRules);
+                var scopedStyle = "";
+
+                rules.forEach(rule => {
+                    if (rule instanceof CSSStyleRule) {
+                        scopedStyle = scopedStyle + this.generateScopedRule(rule);
+                    }
+                    if (rule instanceof CSSMediaRule) {
+                        scopedStyle = scopedStyle + `@media${rule.conditionText} {`
+                        this.listify(rule.cssRules).forEach(rule => {
+                            scopedStyle = scopedStyle + this.generateScopedRule(rule);
+                        });
+                        scopedStyle = scopedStyle + `}`;
+                    }
+                });
+
+                this.addStyle(scopedStyle);
+                style.parentNode.removeChild(style);
+            } else {
+                this.addStyle(this.style);
+            }
+        }
+
+        this.model.removeBanner = this.removeBanner.bind(this, this.app);
+    }
+
+    loaded() {
+        if (this.mounted !== null) this.mounted.call(this.model);
+    }
+
+    removeBanner() {
+        this.app.parentNode.removeChild(this.app);
+    }
+
+    addStyle(css, disabled = false){
+        let style = document.createElement('style');
+        style.type= 'text/css';
+        style.appendChild(document.createTextNode(css)); // doesn't work in IE8 and less
+
+        let inserted = this.app.appendChild(style);
+        inserted.sheet.disabled = disabled;
+        return inserted;
+    }
+
+    getSelectorText(rules) {
+        var ruleList = [];
+        Array.prototype.slice.call(rules).forEach(rule => {
+            ruleList.push(rule);
+        });
+    }
+
+    addAttributes(selector, attr) {
+        this.select(selector).forEach(el => {
+            el.setAttribute(attr, "");
+        })
+    }
+
+    getUuid() {
+        var firstPart = (Math.random() * 46656) | 0;
+        var secondPart = (Math.random() * 46656) | 0;
+        firstPart = ("000" + firstPart.toString(36)).slice(-3);
+        secondPart = ("000" + secondPart.toString(36)).slice(-3);
+        return firstPart + secondPart;
+    }
+
+    generateScopedRule(rule) {
+        let attr = `exp-${this.getUuid()}`;
+        let selector = rule.selectorText;
+        this.addAttributes(selector, attr);
+
+        return `${selector}[${attr}] { ${rule.style.cssText} }`;
+    }
+
     moveMethods() {
-        if (this.methods === undefined) return;
+        if (this.methods === null) return;
 
         for (var key of Object.keys(this.methods)) {
             this.model[key] = this.methods[key];
@@ -41,8 +143,17 @@ class Exp {
     updateModels(key, value) {
         const modelBindings = this.select(`*[exp-model="${key}"]`);
 
-        modelBindings.forEach(el => {
-            el.value = value;
+        modelBindings.forEach(input => {
+            const model = input.getAttribute("exp-model");
+            const type = input.getAttribute("type");
+            
+            if (type == "checkbox") {
+                input.checked = !!value;
+            } else if (type == "radio") {
+                if (input.value == value) input.checked = true;
+            } else {
+                input.value = value;
+            }
         });
     }
 
@@ -77,19 +188,30 @@ class Exp {
     }
 
     bindModels() {
-        let selector = ["email", "number", "search", "tel", "text", "url"].map(input => {
+        let selector = ["email", "number", "search", "tel", "text", "url", "checkbox", "radio"].map(input => {
             return `input[type="${input}"][exp-model]`;
-        })
+        });
+        selector = selector.concat(["textarea[exp-model]", "select[exp-model]"]);
 
         var inputs = this.select(selector.join());
 
         inputs.forEach(input => {
-            let model = input.getAttribute("exp-model");
-            input.value = this.model[model];
-
-            input.addEventListener("input", event => {
-                this.model[model] = event.target.value;
-            });
+            const model = input.getAttribute("exp-model");
+            const type = input.getAttribute("type");
+            
+            if (type === "checkbox") {
+                input.addEventListener("change", event => {
+                    this.model[model] = event.target.checked;
+                });
+            } else if (type === "radio") {
+                input.addEventListener("change", event => {
+                    this.model[model] = event.target.value;
+                });
+            } else {
+                input.addEventListener("input", event => {
+                    this.model[model] = event.target.value;
+                });
+            }
         });
     }
 
@@ -108,14 +230,18 @@ class Exp {
                if (method === null || !(method in that.methods)) return;
 
                el.addEventListener(event, function() {
-                   that.methods[method].apply(that.model);
+                   that.methods[method].apply(that.model, [event]);
                });
            });
        });
     }
 
+    listify(list) {
+        return Array.prototype.slice.call(list);
+    }
+
     select(selector) {
-        return Array.prototype.slice.call(this.app.querySelectorAll(selector));
+        return this.listify(this.app.querySelectorAll(selector));
     }
 }
 
